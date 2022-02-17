5ee4bfa9c5871149147548f8b97204417e97712d
Noah <lauffern@gmail.com>
2022-02-17 18:28:54 +0000

    Linting and testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,25 @@
 const request = require('request');
-const url = 'https://api.thecatapi.com/v1/breeds/search?q='
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
 
-const fetchBreedDescription = function  (breedName, callback) {
+const fetchBreedDescription = function(breedName, callback) {
   
-  request(`${url}${breedName}`, (error, response, body) => { //retrive the url body via request 
+  request(`${url}${breedName}`, (error, response, body) => { //retrive the url body via request
   
     if (error) {
-    callback(error); // Print the error if one  //occurred
+      callback(error); // Print the error if one  //occurred
     } else {
-    const data = JSON.parse(body);
-    if (data[0] === undefined) {
-      return callback(`${breedName} does not exsist.`);
+      const data = JSON.parse(body);
+      if (data[0] === undefined) {
+        return callback(`${breedName} does not exsist.`);
+      }
+      return callback(null, data[0].description);
     }
-    return callback(null, data[0].description);
-  }
     
   });
   
 };
 
 
-module.exports = { fetchBreedDescription }
+module.exports = { fetchBreedDescription };
   
